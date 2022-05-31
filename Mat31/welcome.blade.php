d4fba9eea1e9eacb8aa0032477a9d0e898d75cdc
Basil Abualoush <100359488+BasilAB420@users.noreply.github.com>
2022-05-31 15:23:40 +0300

    Add files via upload

---
+++ Mat31/welcome.blade.php
@@ -0,0 +1,91 @@
+<!doctype html>
+            <html lang="en">
+              <head>
+                <title>Registration</title>
+                <!-- Required meta tags -->
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+                <!-- Bootstrap CSS v5.0.2 -->
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+              </head>
+              <body>
+                <section class="h-100 h-custom" style="background-color: #8fc4b7;">
+                    <div class="container py-5 h-100">
+                      <div class="row d-flex justify-content-center align-items-center h-100">
+                        <div class="col-lg-8 col-xl-6">
+                          <div class="card rounded-3">
+                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/img3.webp"
+                              class="w-100" style="border-top-left-radius: .3rem; border-top-right-radius: .3rem;"
+                              alt="Sample photo">
+                            <div class="card-body p-4 p-md-5">
+                              <h3 class="mb-4 pb-2 pb-md-0 mb-md-5 px-md-2">Registration Info</h3>
+
+                              <form class="px-md-2">
+
+                                <div class="form-outline mb-4">
+                                  <input type="text" id="form3Example1q" class="form-control" />
+                                  <label class="form-label" for="form3Example1q">Name</label>
+                                </div>
+
+                                <div class="row">
+                                  <div class="col-md-6 mb-4">
+
+                                    <div class="form-outline datepicker">
+                                      <input type="text" class="form-control" id="exampleDatepicker1" />
+                                      <label for="exampleDatepicker1" class="form-label">Select a date</label>
+                                    </div>
+
+                                  </div>
+                                  <div class="col-md-6 mb-4">
+
+                                    <select class="select">
+                                      <option value="1" disabled>Gender</option>
+                                      <option value="2">Female</option>
+                                      <option value="3">Male</option>
+                                      <option value="4">Other</option>
+                                    </select>
+
+                                  </div>
+                                </div>
+
+                                <div class="mb-4">
+
+                                  <select class="select">
+                                    <option value="1" disabled>Class</option>
+                                    <option value="2">Class 1</option>
+                                    <option value="3">Class 2</option>
+                                    <option value="4">Class 3</option>
+                                  </select>
+
+                                </div>
+
+                                <div class="row mb-4 pb-2 pb-md-0 mb-md-5">
+                                  <div class="col-md-6">
+
+                                    <div class="form-outline">
+                                      <input type="text" id="form3Example1w" class="form-control" />
+                                      <label class="form-label" for="form3Example1w">Registration code</label>
+                                    </div>
+
+                                  </div>
+                                </div>
+
+                                <button type="submit" class="btn btn-success btn-lg mb-1">Submit</button>
+
+                              </form>
+
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </section>
+
+
+                <!-- Bootstrap JavaScript Libraries -->
+                <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+              </body>
+            </html>
