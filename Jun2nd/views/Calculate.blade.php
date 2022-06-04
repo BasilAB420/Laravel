c86872850c5ddfa1092d1c9deed8d03b38d42dd2
Basil Abualoush <100359488+BasilAB420@users.noreply.github.com>
2022-06-05 01:38:34 +0300

    Update Calculate.blade.php

--- Jun2nd/views/Calculate.blade.php
+++ Jun2nd/views/Calculate.blade.php
@@ -4,17 +4,17 @@
 
 <body>
     <div class="container">
-        <form action="clculate" method="GET">
+        <form action="Calculate" method="GET">
             <h1>Simple Calculator</h1>
             <label>Number 1</label> <br>
             <input type="text" placeholder="0" name="number1" required> <br>
             <label>Number 2</label> <br>
             <input type="text" placeholder="0" name="number2" required> <br>
             <div>
-                <button type="submit" name="adding" class="btns">Add</button>
-                <button type="submit" name="Substract" class="btns">Substract</button>
-                <button type="submit" name="Multiply" class="btns">Multiply</button>
-                <button type="submit" name="Divide" class="btns">Divide</button>
+                <button type="submit" name="adding" >Add</button>
+                <button type="submit" name="Substract" >Substract</button>
+                <button type="submit" name="Multiply">Multiply</button>
+                <button type="submit" name="Divide" >Divide</button>
             </div>
         </form>
         @php
@@ -29,7 +29,7 @@
                 echo '<h5  style=text-align:center;>' . $mul . '</h5>';
             } elseif (isset($_GET['Divide']) == 'submit') {
                 $divi = $_GET['number1'] / $_GET['number2'];
-                echo '<h5  style=text-align:center;>' . $divi . '</h5>';
+                echo '<h5  style=color:red;>'. 'The answer is: ' . $divi . '</h5>';
             }
         @endphp
     </div>
